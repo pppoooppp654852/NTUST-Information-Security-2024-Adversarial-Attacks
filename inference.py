06c542df4a36a898f9e4212a4b85a350fe037e05
MO-CHE, LIAO <pppoooppp654852@gmail.com>
2024-06-01 14:01:06 +0800

    Add inference file and target data

---
+++ inference.py
@@ -0,0 +1,44 @@
+import torch
+from torchvision import transforms
+from pathlib import Path
+from PIL import Image
+import matplotlib.pyplot as plt
+from utils import *
+from model import CustomResNet18
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+transform = transforms.Compose([
+    transforms.Grayscale(num_output_channels=1),
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize([0.5], [0.5])
+])
+
+target_folder = Path("data/target_data")
+
+classes = read_json("config/VIRUS_classes.json")
+
+model = CustomResNet18(num_classes=len(classes))
+model.load_state_dict(torch.load("data/weights/checkpoint.pt"))
+model.to(device)
+model.eval()
+
+for file in target_folder.rglob('*.pt'):
+    label = file.parent.name
+    data = torch.load(file)
+    img = Image.fromarray(data["image"])
+    
+    img_copy = img.copy()
+    img = transform(img).unsqueeze(0).to(device)
+    logits = model(img)
+    probilities = torch.softmax(logits, dim=1)
+    conf, pred = torch.max(probilities, 1)
+    predicted_label = classes[pred.item()]
+    
+    # Display the image with the predicted label
+    plt.figure(figsize=(5,5))
+    plt.imshow(img_copy, cmap='gray')
+    plt.title(f"Label: {label} | Predicted: {predicted_label} | confidence: {conf.item():.4f}", fontsize=8)
+    plt.axis('off')
+    plt.show()
